40c9f270d1466bc0498f64008f81658f5038da94
jago-1009 <garwoodj22@gmail.com>
2023-04-03 18:03:25 -0400

    Updated Homework 8.

--- homework-eight/app/app.js
+++ homework-eight/app/app.js
@@ -53,12 +53,28 @@ var lineupContent = `
         </div>
     </div>
  `
-var productsContent = `<div class="products">
-        <h1> OUR PRODUCTS</h1>
-        <h2>We Have:</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id a, alias quis iusto nam eveniet, ex recusandae ratione corporis facere quisquam dicta ea consectetur dolor illo in error quo eius et? Aliquam accusamus labore, animi officiis tempore, enim totam ducimus ut eligendi quia repudiandae beatae ea omnis ad? Voluptas inventore excepturi, fugit totam iste, pariatur alias, consectetur ea quia maiores et nulla minima facere dolor voluptatum porro sed amet dolorum corrupti consequatur animi! Nulla exercitationem praesentium dicta quam excepturi veritatis eius nemo inventore sit possimus. Quae quasi nam aliquid quo ducimus voluptatum eius aspernatur quos animi, dignissimos esse provident id.</p>
-        <div class="productimg"></div>
-    </div>`
+var productsContent = ` <div class="products">
+    <h1> OUR PRODUCTS</h1>
+    <h2>We Have:</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id a, alias quis iusto nam eveniet, ex recusandae ratione
+      corporis facere quisquam dicta ea consectetur dolor illo in error quo eius et? Aliquam accusamus labore, animi
+      officiis tempore, enim totam ducimus ut eligendi quia repudiandae beatae ea omnis ad? Voluptas inventore excepturi,
+      fugit totam iste, pariatur alias, consectetur ea quia maiores et nulla minima facere dolor voluptatum porro sed amet
+      dolorum corrupti consequatur animi! Nulla exercitationem praesentium dicta quam excepturi veritatis eius nemo
+      inventore sit possimus. Quae quasi nam aliquid quo ducimus voluptatum eius aspernatur quos animi, dignissimos esse
+      provident id.</p>
+    <div class="productimg"></div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dignissimos neque, eos porro maiores reiciendis dolores necessitatibus natus, iure hic atque rem! Consequuntur corrupti fuga in inventore soluta? Odio a error blanditiis? Recusandae, provident delectus incidunt quo in quod aliquid vitae odio saepe tenetur quos ipsa ab, officiis eligendi nam non a. Vero totam nobis dolorum illum maiores nostrum? Recusandae dolor repellat error vel, corrupti amet similique fugiat iure voluptatibus laboriosam magnam nemo itaque minus cupiditate aliquam ratione veniam nostrum deserunt asperiores modi! In, nostrum numquam? Repellat, facere, est dolores molestias odio dolorum aliquid laboriosam fugit necessitatibus nobis vero accusamus.
+      </p>
+      <div class="product-2"></div>
+      <div class="product-2-text"><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, fugiat officia consequatur ipsa optio mollitia ab fuga iure culpa incidunt? Similique, non ea dolores dignissimos voluptate, quasi odio hic soluta et perspiciatis ratione molestias consequatur explicabo nisi aliquam veniam voluptatibus sequi quas id a ad rem alias labore animi! Nam, eveniet consequatur iste ipsam ratione veritatis id architecto? Sit, possimus assumenda corrupti accusamus dicta corporis voluptate mollitia natus eveniet repellat quod iure sapiente dolorem? Velit dolores fugiat, ipsum cupiditate dicta illo voluptate expedita perspiciatis, earum hic nam, obcaecati quaerat. Tempora animi sapiente cupiditate, enim facere laudantium veritatis beatae, odit quis id, dolores architecto excepturi quod doloremque amet dolorum neque aut inventore iure repellendus omnis minus fuga! Sapiente vero maxime molestias et fugiat iste quam, excepturi saepe, deleniti dignissimos obcaecati in voluptates voluptate placeat ea ipsum eum similique, optio cum necessitatibus? Hic ipsum obcaecati omnis animi? Perferendis est eum quae accusantium, dolorum quo, commodi temporibus hic, natus magnam rem illum eaque iure velit vero repellendus perspiciatis modi ullam ad. In quas fuga adipisci quo ipsa, magnam animi at nam quidem autem. Ipsam pariatur, distinctio, culpa natus fugit repellendus ullam quod ipsum voluptates voluptatem dicta nemo dolorum? Deserunt inventore provident adipisci pariatur.</p></div>
+      <div class="product-side-by-side">
+        <h2>NEW!</h2>
+        <div class="product-left"></div>
+        <div class="product-right"><p></p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam reiciendis magnam, optio at maiores culpa deleniti non eum id quidem necessitatibus, vitae cum tenetur repudiandae fuga veniam alias ipsa tempore consectetur rem inventore sed vero reprehenderit? Alias voluptatibus dignissimos nesciunt illo. In, expedita! Nam ex eos asperiores culpa? Corrupti a ad labore velit, incidunt nobis ipsa repudiandae non in expedita, animi soluta. Quia mollitia sed culpa. Vitae veniam, iste velit aut esse fugit similique eos perferendis obcaecati impedit reprehenderit voluptates quibusdam nisi corrupti ratione ad voluptate labore commodi. Quibusdam sequi magni veniam. Omnis laboriosam, quos dolores deleniti vero repellat quaerat. Doloribus dolor officia aliquam ad ex explicabo vel numquam impedit! Aut odio consequatur cum minima dicta explicabo error doloribus accusamus nam magni rem autem, quidem eum sapiente perferendis eius nemo. Enim eius nemo ratione dignissimos repellat vero quaerat labore consequuntur perspiciatis totam inventore accusamus incidunt, ut nulla, officiis amet unde maxime. Minus animi sint similique delectus vel adipisci veritatis molestiae quia quidem temporibus quam facere dignissimos libero fugit dicta, ex minima quis sit voluptas repudiandae totam ducimus commodi corporis. Quis impedit laboriosam, odit ipsa illo, nihil saepe ea atque unde minima nostrum esse veniam corporis commodi, eos optio a. Doloribus!</p></div>
+      </div>
+
+  </div>`
     var contactContent = `<div class="contact">
     <h1>Contact Us</h1>
     <h2>You can contact us by:
